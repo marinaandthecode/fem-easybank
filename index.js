9583f911dd60fc0ec08d273977e000c614666fa7
Marina Vaseniova <mvasenyova@gmail.com>
2021-12-16 05:37:14 +0300

    Fix mobile menu behavior: close on click outside

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const hamburgerButton = document.querySelector(".header__hamburger");
 const menu = document.querySelector(".menu"); 
 
 hamburgerButton.addEventListener('click', function(){
+    event.stopPropagation();
     if (menu.classList.contains('is-active')) {
         menu.classList.remove('is-active');
         hamburgerButton.classList.remove('is-active');
@@ -10,3 +11,10 @@ hamburgerButton.addEventListener('click', function(){
         hamburgerButton.classList.add('is-active');
     }
 });
+
+document.addEventListener('click', function() {
+    if (hamburgerButton.classList.contains('is-active')) {
+        menu.classList.remove('is-active');
+        hamburgerButton.classList.remove('is-active');
+    }
+})
